87c0d9dc14df70142a3b22d463f043b7bf8f5764
szymon_kubuntu <bogusszymon2000@gmail.com>
2022-05-09 22:23:38 +0200

    Added users database handler for filling users table with data. TODO: add client handler and change utilities so they work with sql commands

--- test.py
+++
@@ -1,6 +0,0 @@
-from utils.usr import User
-
-
-u_id = '111'
-usr = User(u_id)
-print(type(usr))
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -5,5 +5,20 @@ from dataclasses import dataclass
 class Items:
     """Class which stores importnt variables, paths etc.."""
 
+    # ______paths_______________________
     users_rootdir: str = '/home/szymon_kubuntu/Documents/myProjects/Be_crudAPI-copy/users/'
     users_configs: str = '/home/szymon_kubuntu/Documents/myProjects/Be_crudAPI-copy/configs/'
+    users_config_json: str = '/home/szymon_kubuntu/Documents/myProjects/Be_crudAPI-copy/users/users_config.json'
+
+    # ______database management_________
+    database_configs = {
+        'host': 'localhost',
+        'port': '3306',
+        'usr': 'szymon',
+        'password': '',
+        'db_name': 'be_api'
+    }
+    db_usrs_data: str = 'mysql+pymysql://' + database_configs['usr'] + '@' + database_configs['host']\
+                   + ':' + database_configs['port'] + '/' + database_configs['db_name']
+    usrs_db_name: str = 'Users'
+    clients_db_name: str = 'Clients'
